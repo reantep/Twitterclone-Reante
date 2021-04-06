a77ba101584d5288e0fc45ee25fc530918050008
reante patterson <reantepatterson@gmail.com>
2021-04-06 02:30:38 -0700

    Fixed a few bugs with ui, added like button

--- TwitterClone/TwitterClone/settings.py
+++ TwitterClone/TwitterClone/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR , "static"),
+    BASE_DIR / "static",
 ]
 
 cloudinary.config( 
